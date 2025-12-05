b0c5c96e22a795589c1490e866b90c99c0e5c3fd
wjm99 <1371746602@qq.com>
2025-12-05 16:34:00 +0100

    load glued info after start

--- Glued/ContentView.swift
+++ Glued/ContentView.swift
@@ -13,10 +13,13 @@ struct ContentView: View {
     @StateObject var fetcher = DeviceInfoFetcher()
 
     @State private var selectedAddress: String? = nil
+    @State private var gluedDevice: GluedDevice? = nil
 
     var body: some View {
     VStack(alignment: .leading) {
-        Text("Glue to: ").font(.title2).bold()
+        Text("Glued to: \(gluedDevice?.name ?? "None")")
+            .font(.system(.title2, design: .monospaced))
+            .bold()
         
         GroupBox {
             List(fetcher.deviceinfos, id: \.address) { info in
@@ -36,9 +39,12 @@ struct ContentView: View {
                     .onTapGesture {
                         if selectedAddress == info.address {
                             selectedAddress = nil
+                            GluedDevice.clear()
+                            gluedDevice = nil
                         } else {
                             selectedAddress = info.address
                             GluedDevice.save(from: info)
+                            gluedDevice = GluedDevice.load()
                         }
                     }
                 
@@ -49,6 +55,11 @@ struct ContentView: View {
     .padding()
     .fixedSize(horizontal: false, vertical: true)
     .task {
+        gluedDevice = GluedDevice.load()
+        if let device = gluedDevice {
+            selectedAddress = device.address
+        }
+        
         do {
             fetcher.deviceinfos = try await fetcher.getDeviceInfo()
         } catch {
